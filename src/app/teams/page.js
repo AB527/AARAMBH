500c57ecf658cf7f14ed8c7f1a341a90f0717b63
AB527 <atharvabedekar527@gmail.com>
2024-10-18 15:19:36 +0530

    Commit

---
+++ src/app/teams/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import "./style.css"
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+        <p className="text-3xl">Renegades</p>
+        <p className="good" >Renegadesa</p>
+    </>
+  );
+}
