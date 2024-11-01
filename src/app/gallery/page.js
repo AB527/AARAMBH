fec59a0b18264a2954403f1a4b0d99e5449592b5
AB527 <atharvabedekar527@gmail.com>
2024-11-01 17:08:47 +0530

    Update page.js

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -7,63 +7,6 @@ import { useEffect, useState } from "react";
 import Loading from "../components/Loading"
 import { fetchAPI } from "../utility";
 
-const dataX = [
-  {
-    img: "1.jpg",
-    title: "Day 1: Cricket",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "2.jpg",
-    title: "Day 3: Chess",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "3.jpg",
-    title: "Day 2: Cricket",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "4.jpg",
-    title: "Day 3: Basketball",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "5.jpg",
-    title: "Day 3: Volleyball",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "5.jpg",
-    title: "Day 3: Volleyball",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "5.jpg",
-    title: "Day 3: Volleyball",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "5.jpg",
-    title: "Day 3: Volleyball",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  },
-  {
-    img: "5.jpg",
-    title: "Day 3: Volleyball",
-    imgnum: 10,
-    link: "https://drive.google.com/drive/folders/1sLOlICgwImh95KpYP0TnBMIjAtTAtS20?usp=drive_link"
-  }
-]
-
 export default function Page() {
   const [data, setData] = useState([])
   useEffect(() => {
@@ -78,14 +21,14 @@ export default function Page() {
         data.length ?
           <div class="grid grid-cols-1 mx-5 sm:grid-cols-2 sm:mx-0 lg:grid-cols-4 gap-2">
             {
-              data.map((img, i) => (
-                <Link key={i} className="relative" href={img.link}>
+              data.map((view, i) => (
+                <Link key={i} className="relative" href={view.gallery_link} target="_blank">
                   <div className="absolute top-2 left-3 flex items-center justify-center bg-black px-2 py-1 rounded-2xl opacity-75">
-                    <Image height={1000} width={1000} className="h-4 w-5 rounded-sm" src={`/icons/camera.png`} alt={img.title} />
-                    <p className="text-sm font-bold text-white pl-1 text-center">{img.imgnum}</p>
+                    <Image height={1000} width={1000} className="h-4 w-5 rounded-sm" src={`/icons/camera.png`} alt={view.title} />
+                    <p className="text-sm font-bold text-white pl-1 text-center">{view.img_num}</p>
                   </div>
-                  <Image height={1000} width={1000} className="h-52 w-96 rounded-md" src={`/images/gallery/${img.img}`} alt={img.title} />
-                  <p className="text-lg sm:text-lg font-bold text-white absolute bottom-2 left-3 font-sans">{img.title}</p>
+                  <Image height={1000} width={1000} className="h-52 w-96 rounded-md" src={`/images/gallery/${view.thumbnail}`} alt={view.title} />
+                  <p className="text-lg sm:text-lg font-bold text-white absolute bottom-2 left-3 font-sans">{view.title}</p>
                 </Link>
               ))
             }
